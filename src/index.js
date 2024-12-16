c34812c2909d9ff343a63cfc9d87baef0d0532c7
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2024-12-16 15:17:16 +0530

    added body parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const server = require('./config/ServerConfig')
 const connectDB = require('./config/dbConfig')
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded);
+app.post('/ping',(req,res)=>{
+    console.log(req.body)
+    return res.json({message: 'pong'})
+})
 app.listen(server.PORT, async ()=>{
     await connectDB();
     console.log(`Port started at ${server.PORT} !!`)
